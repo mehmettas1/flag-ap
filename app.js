00c318822244ba0c2b5b0f738e27e3c5c8324aa4
mehmet <tas349873@gmail.com>
2022-06-19 00:08:59 +0300

    js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 //*                     FLAG-APP
 //*=========================================================
 
-const fetchCountry = () => {
+const fetchCountry = async(name) => {
     const url =`https://restcountries.com/v3.1/name/${name}`;
 
    try {
@@ -11,19 +11,16 @@ const fetchCountry = () => {
     if (!res.ok) {
         renderError(`Something went wrong :${res.status}`)
         throw new Error();
-        renderCountry(data[0]);
+        
     }
     const data = await res.json();
-    console.log(data[0]);
+    renderCountry(data[0]);
+    // console.log(data[0]);
    } catch (error) {
   console.log(error);
    }
+  };
 
-
-
-
-  
-  
    const renderError = (err)=> {
     const countriesDiv = document.querySelector(".countries")
 
@@ -31,21 +28,45 @@ const fetchCountry = () => {
   <h1 class="text-danger">${err}</h1>
     <img src="./img/484.png" alt ="" />
     `;
-   };
+   
+  };
 
 
    const renderCountry = (country)=> {
       console.log(country);
       const countriesDiv = document.querySelector(".countries")
-      const{capital,} = country;
-      console.log(capital);
-   }
+      const{capital,
+        name :{common},
+        region,flags:{svg},
+        languages,
+        currencies,
+      } = country;
+      // newFunction(capital, common, region, svg);
+     countriesDiv.innerHTML += `
 
-};
+     <div class="card shadow-lg" style="width: 18rem;">
+     <img src="${svg}" class="card-img-top" alt="...">
+     <div class="card-body">
+       <h5 class="card-title">${common}</h5>
+       <p class="card-text">${region}</p>
+     </div>
+     <ul class="list-group list-group-flush">
+       <li class="list-group-item"> <i class="fas fa-lg fa-landmark"></i> ${capital}</li>
+       <li class="list-group-item"><i class="fas fa-lg fa-comments"></i> ${Object.values(languages)}</li>
+       <li class="list-group-item"><i class="fas fa-lg fa-money-bill-wavw"></i> ${Object.values(currencies)[0].name},${Object.values(currencies)[0].symbol}</li>
+     </ul>
+     
+   </div>
+   `;
+
+
+   };
+
+   fetchCountry('turkey');
+   fetchCountry('usa');
 
 
 
 
 
-fetchCountry("turkey");
-fetchCountry("usa");
+
