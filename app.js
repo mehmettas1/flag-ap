5a65758755e70a7085a3d41ed08e3a18d11c685d
mehmet <tas349873@gmail.com>
2022-07-03 17:12:46 +0300

    the end

--- app.js
+++ app.js
@@ -2,71 +2,80 @@
 //*                     FLAG-APP
 //*=========================================================
 
-const fetchCountry = async(name) => {
-    const url =`https://restcountries.com/v3.1/name/${name}`;
+const select = document.getElementById("select");
+const dive = document.getElementById("dive");
+const option = document.querySelectorAll("option");
 
-   try {
-    const res = await fetch (url);
+bringCountry();
+async function bringCountry() {
+  const url = `https://restcountries.com/v3.1/all `;
+
+  try {
+    const res = await fetch(url);
 
     if (!res.ok) {
-        renderError(`Something went wrong :${res.status}`)
-        throw new Error();
-        
+      fonkerror(`Something went wrong:${res.status}`);
+      throw new Error();
     }
     const data = await res.json();
-    renderCountry(data[0]);
-    // console.log(data[0]);
-   } catch (error) {
-  console.log(error);
-   }
-  };
-
-   const renderError = (err)=> {
-    const countriesDiv = document.querySelector(".countries")
-
-    countriesDiv.innerHTML= `
-  <h1 class="text-danger">${err}</h1>
-    <img src="./img/484.png" alt ="" />
-    `;
-   
-  };
+    let countryList = [];
+    data.forEach((user) => {
+      select.innerHTML += `<option value="${user.name.common}">${user.name.common}</option>`;
+    });
+ 
+  } catch (error) {
+    console.log(error);
+  }
+}
+   select.addEventListener("change", (e) => {
+      console.log(e.target.value);
+      send(e.target.value);
+    });
 
 
-   const renderCountry = (country)=> {
-      console.log(country);
-      const countriesDiv = document.querySelector(".countries")
-      const{capital,
-        name :{common},
-        region,flags:{svg},
-        languages,
+
+function fonkerror(msg) {
+  console.log(msg);
+}
+
+const send = (country) => {
+
+
+   const url = `https://restcountries.com/v2/name/${country}`;
+
+ 
+    const res = fetch(url).then((res)=> res.json())
+    .then((country)=>{
+      console.log(country[0]);
+      
+      const {
+        capital,
+        name,
+        region,
+     languages,
+        flags: { svg },
         currencies,
-      } = country;
-      // newFunction(capital, common, region, svg);
-     countriesDiv.innerHTML += `
-
-     <div class="card shadow-lg" style="width: 18rem;">
-     <img src="${svg}" class="card-img-top" alt="...">
-     <div class="card-body">
-       <h5 class="card-title">${common}</h5>
-       <p class="card-text">${region}</p>
-     </div>
-     <ul class="list-group list-group-flush">
-       <li class="list-group-item"> <i class="fas fa-lg fa-landmark"></i> ${capital}</li>
-       <li class="list-group-item"><i class="fas fa-lg fa-comments"></i> ${Object.values(languages)}</li>
-       <li class="list-group-item"><i class="fas fa-lg fa-money-bill-wavw"></i> ${Object.values(currencies)[0].name},${Object.values(currencies)[0].symbol}</li>
-     </ul>
-     
-   </div>
-   `;
+      } = country[0];
+      console.log(name,capital,region,svg,currencies,languages);
+  dive.innerHTML = `      
+      <img src="${svg}" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">${name} </h5>
+        <p class="card-text">${capital}</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">${region}</li>
+        <li class="list-group-item">${languages[0].name}</li>
+        <li class="list-group-item">${Object.values(currencies)[0].name},${
+    Object.values(currencies)[0].symbol
+  }</li>
+      </ul>`;
 
 
-   };
-
-   fetchCountry('turkey');
-   fetchCountry('usa');
+    })
 
 
+ 
+    
 
-
-
-
+};
